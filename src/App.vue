e296eac65cfef7ecb3daa0ce4e33a33151063fab
elliot404 <fortesting40404@gmail.com>
2021-04-29 13:49:33 +0530

    init

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Main />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Main from './components/Main.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Main
   }
 }
 </script>
@@ -22,5 +22,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+body {
+  display: grid;
+  place-content: center;
 }
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,17 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    toggle: false
   },
   mutations: {
+    toggle(state) {
+      state.toggle = !state.toggle
+    }
   },
   actions: {
+    toggle({commit}) {
+      commit('toggle')
+    }
   },
   modules: {
   }
